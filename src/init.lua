dfaf6ecb044161ab11a98c039d11c154895566e8
jeparlefrancais <jeparlefrancais21@gmail.com>
2024-08-30 12:37:01 -0400

    Export prop types from each component

--- src/init.lua
+++ src/init.lua
@@ -1,10 +1,24 @@
+local Action = require('./Action')
+local Menu = require('./Menu')
+local RobloxPlugin = require('./RobloxPlugin')
+local Toolbar = require('./Toolbar')
+local ToolbarButton = require('./ToolbarButton')
+local Widget = require('./Widget')
+
+export type ActionProps = Action.Props
+export type MenuProps = Menu.Props
+export type RobloxPluginProps = RobloxPlugin.Props
+export type ToolbarProps = Toolbar.Props
+export type ToolbarButtonProps = ToolbarButton.Props
+export type WidgetProps = Widget.Props
+
 local RobloxStudioPlugin = {
-    Action = require('./Action'),
-    Menu = require('./Menu'),
-    RobloxPlugin = require('./RobloxPlugin'),
-    Toolbar = require('./Toolbar'),
-    ToolbarButton = require('./ToolbarButton'),
-    Widget = require('./Widget'),
+    Action = Action,
+    Menu = Menu,
+    RobloxPlugin = RobloxPlugin,
+    Toolbar = Toolbar,
+    ToolbarButton = ToolbarButton,
+    Widget = Widget,
     usePlugin = require('./hooks/usePlugin'),
     useWidgetKeyDown = require('./hooks/useWidgetKeyDown'),
     useWidgetFocus = require('./hooks/useWidgetFocus'),
